36efa6f035b9fb6882fd763825a1d7c4b6ec7380
JuanDaDiaz1005 <juan.1701920068@ucaldas.edu.co>
2024-05-17 13:45:39 -0500

    El juego está terminado, sólo falta añadir localStorage para guardar partida

---
+++ projects/01_tic_tac_toe/src/logic/board.js
@@ -0,0 +1,19 @@
+import { winningCombos } from '../constants'
+
+export const checkWinner = (boardToCheck) => {
+    for (const combo of winningCombos) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] && 
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    return null
+  }
+
+export const checkEndGame = (boardToCheck) => {
+    return boardToCheck.every((square) => square !== null)
+  }  
